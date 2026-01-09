fa6dbe789682adc95e8399417cab6ef258a63cbf
Aman Singh Rathore <amansinghrathore917@gmail.com>
2026-01-09 19:44:01 +0000

    Add variable and datatype examples with explanations for var, let, and const

--- 01_Basics/02_Datatyps.js
+++ 01_Basics/02_Datatyps.js
@@ -56,8 +56,42 @@ console.log('null to number: ' + nullToNumber);
 console.log(typeof nullToNumber);  
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+let neail = true;
+
+let nulltostr = String(neail);
 
 
+
+console.log(nulltostr)
+
+
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 
 
