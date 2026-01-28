a61bb394fcf2c2c38998c916577898e6ad949281
Aman Singh Rathore <amansinghrathore917@gmail.com>
2026-01-28 06:19:29 +0530

    Control flow in javascript

---
+++ 04_controle flow/One.js
@@ -0,0 +1,51 @@
+//
+
+// if (true) { than your code exicuted.
+    
+// }
+
+//comparition;
+
+if (2 == 2) {
+    console.log('true')
+}
+
+let tamprature = 41;;
+
+if (tamprature < 50) {
+    console.log('less than 50')
+} else {
+    console.log('grater than 50')
+}
+
+// <,>,<=,>=, ,==,===,!=,!===
+
+const score = 20;
+if (score > 100) {
+    const power = "fly";
+    console.log(`User power: ${power}`);
+    
+    
+} else {
+    console.log(`User don't have no power`);
+}
+
+const balance = 999;
+
+if (balance <= 1000) {
+    console.log('your balance less than 1000 in your account balnce right now is;-', balance );
+    
+} else {
+    console.log('its ok')
+}
+
+const userlogdin = true;
+const userDabidCard = true;
+if (userDabidCard && userlogdin && 1) {
+    console.log('evrthing ok');
+} else {
+    console.log('you are fk indian frude');
+}
+
+
+
