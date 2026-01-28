446161a5231f6495b4b6f243594724b0f194a5c7
Aman Singh Rathore <amansinghrathore917@gmail.com>
2026-01-28 07:10:53 +0530

    just wow

---
+++ 04_controle flow/Switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+let month = 10;
+switch (month) {
+  case month === 10:
+    console.log("that's jan");
+    break;
+  case "feb":
+    console.log("that's february");
+    break;
+  case "march":
+    console.log("that's march");
+    break;
+  case "april":
+    console.log("that's april");
+    break;
+
+  default:
+    console.log("that's some another month i don't know");
+    break;
+}
