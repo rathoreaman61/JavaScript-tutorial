179381ddaeaf4f60d1e2d9cf4d6f2eabd0ef81c7
Aman Singh Rathore <amansinghrathore917@gmail.com>
2026-02-04 19:09:59 +0530

    for each

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,2 +1,21 @@
 let conding = ['js','ruby','swift','ruby','cpp','c'];
-conding.forEach((itam)=>( return conding;))
+conding.forEach((itam)=>{console.log(itam)});
+
+const myCoding = [
+    {
+languageName: "javascript",
+languageFileName: "js"
+},
+
+{
+languageName: "java",
+languageFileName: "java"
+},
+
+ {
+languageName: "python",
+languageFileName: "py"
+}   
+]
+    
+    myCoding.forEach((itam,index,array)=>(console.log(itam.languageName)));
