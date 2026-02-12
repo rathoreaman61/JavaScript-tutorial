ceb71db2c1d0a611a450aa535eb9bf5add0a94a6
Aman Singh Rathore <amansinghrathore917@gmail.com>
2026-02-13 02:30:14 +0530

    again start

---
+++ 05_iterations/nine.js
@@ -0,0 +1,8 @@
+const array = [1,2,3,4];
+
+const initiailValue = 0;
+
+const arrayReduce = array.reduce((intial,array)=>{console.log(`intia= ${intial} + array ${array} `)
+return intial + array },initiailValue);
+
+
