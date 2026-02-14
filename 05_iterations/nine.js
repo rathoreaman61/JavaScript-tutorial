4a83e03505b5e1b54b81825204d9322ace56c3f7
Aman Singh Rathore <amansinghrathore917@gmail.com>
2026-02-14 23:44:09 +0530

    strting dom

--- 05_iterations/nine.js
+++ 05_iterations/nine.js
@@ -6,3 +6,26 @@ const arrayReduce = array.reduce((intial,array)=>{console.log(`intia= ${intial} 
 return intial + array },initiailValue);
 
 
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+];
+
+const priceToPay = shoppingCart.reduce((acc,value)=>(acc + value.price),0);
+
+console.log(priceToPay);
+
