598eff786d40cf126b02f53cbdaa6baa119a8dd5
Aman Singh Rathore <amansinghrathore917@gmail.com>
2026-02-18 17:25:50 +0000

    refactor: update color changing logic for title element

--- 06_DOM/two.js
+++ 06_DOM/two.js
@@ -1,5 +1,14 @@
+const changedColor = document.querySelector('#title');
+changedColor.style.color = 'purple';
 
-const h1_BgColor_Color = document.querySelector('h1[id = "title"]');
-h1_BgColor_Color.style.color = 'purple'
+const colors = ['yellow', 'green', 'blue'];
+let index = 0;
 
+const interval = setInterval(() => {
+   changedColor.style.color = colors[index];
+   index = (index + 1) % colors.length;
+}, 1000);
 
+setTimeout(() => {
+   clearInterval(interval);
+}, 10000);
